b0456cafdecaf2ea14a1916799bd4fe4273a5e29
Anagha Burki S <anagha.burki@gmail.com>
2025-02-08 16:13:03 +0530

    Signup

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,5 +1,56 @@
-export const signup = (req,res) =>{
-    res.send("Signup Route")
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.models.js"
+import bcrypt from "bcryptjs"
+
+
+export const signup = async (req,res) =>{
+    const{fullName,email,password} = req.body
+    try{
+        if(!fullName || !email || !password){
+            return res.status(400).json({msg: 'All fields are required'});
+
+        }
+
+
+
+        if(password.length <6) {
+            return res.status(400).json({msg: 'Password must be at least 6 characters'});
+        }
+
+        const user = await User.findOne({email})
+
+        if (user) return res.status(400),json({message: "mail already exists"});
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new User({
+            fullName,
+            email,
+            password: hashedPassword
+        })
+
+
+        if(newUser){
+            generateToken(newUser._id,res)
+            await newUser.save()
+
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic: newUser.profilePic,
+            });
+
+        } else{
+            return res.status(400).json({msg: 'Invalid User data'})
+        }
+
+        
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({msg: 'Internal Server Error'})
+    }
 };
 
 export const login = (req,res) =>{
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,9 @@ import authRoutes from "./routes/auth.routes.js"
 dotenv.config()
 const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
+
+app.use(express.json())
 
 app.use("/api/auth", authRoutes)
 app.listen(PORT, ()=> {
