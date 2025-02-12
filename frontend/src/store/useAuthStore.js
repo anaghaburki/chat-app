7fff6b06282d1ed003369a5915a578ee15d6e5a7
Anagha Burki S <anagha.burki@gmail.com>
2025-02-12 19:04:12 +0530

    Profile Page

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -69,4 +69,18 @@ export const useAuthStore = create((set) =>({
             set({ isLoggingin: false });
         }
     },
+
+    updateProfile: async (data) => {
+        set({ isUpdatingProfile: true });
+        try {
+          const res = await axiosInstance.put("/auth/update-profile", data);
+          set({ authUser: res.data });
+          toast.success("Profile updated successfully");
+        } catch (error) {
+          console.log("error in update profile:", error);
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isUpdatingProfile: false });
+        }
+      },
 }));
